c1a4debb670a8a6840384b0ef5dece8da242f889
vinayakg17 <86881531+vinayakg17@users.noreply.github.com>
2023-02-03 15:15:28 +0530

    Update CompanyRepository.java

--- src/main/java/com/innoventes/test/app/repository/CompanyRepository.java
+++ src/main/java/com/innoventes/test/app/repository/CompanyRepository.java
@@ -1,10 +1,13 @@
 package com.innoventes.test.app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.innoventes.test.app.entity.Company;
 
 
 public interface CompanyRepository extends JpaRepository<Company, Long> {
+
+	public Company getByCompanyCode(String code);
 	
 }
